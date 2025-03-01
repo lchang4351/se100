402d1fddf67e308bfe18d1ec21ed248e9f779f47
lchang4351 <lchang4351@gmail.com>
2025-03-01 09:07:19 +0800

    update start of L4

---
+++ L3/question1.js
@@ -0,0 +1,11 @@
+const msftData = [190.15, 194.21, 191.07, 190.44, 200.15];
+
+// Write your code below
+
+let total = 0;
+for(let i=0; 1<msftData.length; i++) {
+
+    total = total + msftData[i];
+}
+
+console.log(total/msftData.length)
